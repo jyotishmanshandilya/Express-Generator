1a18e11e6d19fddb82b64149a03690ad9638529c
jyotishman <jyotishmanshandilyaggn@gmail.com>
2023-01-02 22:48:01 +0800

    File uploads

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -71,6 +72,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leader', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 
 // catch 404 and forward to error handler
